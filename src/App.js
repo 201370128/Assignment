d1b99c04e502c0dc29bceac3ecac5447140833e2
khizar872 <201370128@gift.edu.pk>
2023-08-25 23:57:17 +0500

    initial

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Muhamad Taha Shahid</h1>
+        <h1>201370128</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
